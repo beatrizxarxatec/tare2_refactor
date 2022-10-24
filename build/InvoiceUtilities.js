41f92d1018ac722caa579d2f67641053911201f6
beatrizxarxatec <beatriz.rodriguez@xarxatecactiva.com>
2022-10-24 18:29:39 +0200

    agregue cambios

--- build/InvoiceUtilities.js
+++ build/InvoiceUtilities.js
@@ -1,40 +1,49 @@
 export class InvoiceUtilities {
     static printInvoice(invoices, plays) {
-        let totalAmount = 0; //  monto total(consumo total en tu targeta de crédito)
+        let totalAmount = 0; //  monto total
         let volumeCredits = 0;
         let result = `Detalle de factura para ${invoices[0].customer}\n`; // customer(cliente) currency(moneda)
-        const format = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR", minimumFractionDigits: 2 }).format;
-        for (let perf of invoices[0].performances) { // tipo performance (arte corporal(el movimiento Fluxus, el happening o el arte intermedia) )
+        for (let perf of invoices[0].performances) { // performarce(actuación) construir lo que se va sacando por la consola
             const play = plays[perf.playID];
-            let thisAmount = 0;
-            switch (play.type) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.audience > 30) {
-                        thisAmount += 1000 * (perf.audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.audience - 20);
-                    }
-                    thisAmount += 300 * perf.audience;
-                    break;
-                default:
-                    throw new Error(`Tipo desconocido: ${play.type}`);
-            }
-            // add volume credits
-            volumeCredits += Math.max(perf.audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy" === play.type)
-                volumeCredits += Math.floor(perf.audience / 5);
+            const thisAmount = InvoiceUtilities.CalculatePaymentAmount(perf.audience, play.type);
+            const thisCredits = InvoiceUtilities.CalculateCredits(perf.audience, play.type);
             // print line for this order
-            result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience} asientos)\n`;
+            result += `${play.name}: ${InvoiceUtilities.format(thisAmount / 100)} (${perf.audience} asientos)\n`;
+            // add volume credits
+            volumeCredits += thisCredits;
             totalAmount += thisAmount;
         }
-        result += `Total a pagar ${format(totalAmount / 100)}\n`;
+        result += `Total a pagar ${InvoiceUtilities.format(totalAmount / 100)}\n`;
         result += `Has ganado ${volumeCredits} creditos\n`;
         return result;
     }
+    static CalculateCredits(audience, playType) {
+        let thisCredits = Math.max(audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === playType)
+            thisCredits += Math.floor(audience / 5);
+        return thisCredits;
+    }
+    static CalculatePaymentAmount(audience, playType) {
+        let thisAmount = 0;
+        switch (playType) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (audience > 30) {
+                    thisAmount += 1000 * (audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (audience > 20) {
+                    thisAmount += 10000 + 500 * (audience - 20);
+                }
+                thisAmount += 300 * audience;
+                break;
+            default:
+                throw new Error(`Tipo desconocido: ${playType}`);
+        }
+        return thisAmount;
+    }
 }
+InvoiceUtilities.format = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR", minimumFractionDigits: 2 }).format; // format es una función que te permite formatear números
--- src/InvoiceUtilities.ts
+++ src/InvoiceUtilities.ts
@@ -1,41 +1,54 @@
-import { Invoice, Play } from "./types";
+import { Invoice, Performance, Play } from "./types";
 
- export class InvoiceUtilities {   
-    static printInvoice (invoices:Invoice[], plays:Record<string, Play>) { // facturas y obras de teatro
+export class InvoiceUtilities {
+    static format = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR", minimumFractionDigits: 2 }).format; // format es una función que te permite formatear números
+
+    static printInvoice(invoices: Invoice[], plays: Record<string, Play>) { // facturas y obras de teatro
         let totalAmount = 0; //  monto total
         let volumeCredits = 0;
         let result = `Detalle de factura para ${invoices[0].customer}\n`; // customer(cliente) currency(moneda)
-        const format = new Intl.NumberFormat("de-DE",{ style: "currency", currency: "EUR", minimumFractionDigits: 2 }).format;
-        for (let perf of invoices[0].performances) { // performarce(actuación)) )
+        for (let perf of invoices[0].performances) { // performarce(actuación) construir lo que se va sacando por la consola
             const play = plays[perf.playID];
-            let thisAmount = 0;
-            switch (play.type) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.audience > 30) {
-                        thisAmount += 1000 * (perf.audience-30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.audience - 20);
-                    }
-                    thisAmount += 300 * perf.audience;
-                    break;
-                default:
-                    throw new Error(`Tipo desconocido: ${play.type}`);
-            }
-            // add volume credits
-            volumeCredits += Math.max(perf.audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+            const thisAmount = InvoiceUtilities.CalculatePaymentAmount(perf.audience, play.type);
+            const thisCredits = InvoiceUtilities.CalculateCredits(perf.audience, play.type);
             // print line for this order
-                result += `${play.name}: ${format(thisAmount/100)} (${perf.audience} asientos)\n`;
-                totalAmount += thisAmount;
+            result += `${play.name}: ${InvoiceUtilities.format(thisAmount / 100)} (${perf.audience} asientos)\n`;
+            // add volume credits
+            volumeCredits += thisCredits;
+            totalAmount += thisAmount;
         }
-        result += `Total a pagar ${format(totalAmount/100)}\n`;
+        result += `Total a pagar ${InvoiceUtilities.format(totalAmount / 100)}\n`;
         result += `Has ganado ${volumeCredits} creditos\n`;
         return result;
+    }
+
+    private static CalculateCredits(audience: number, playType: "comedy" | "tragedy") {
+        let thisCredits = Math.max(audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === playType)
+            thisCredits += Math.floor(audience / 5);
+        return thisCredits;
+    }
+
+    private static CalculatePaymentAmount(audience: number, playType: "comedy" | "tragedy") {
+        let thisAmount = 0;
+        switch (playType) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (audience > 30) {
+                    thisAmount += 1000 * (audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (audience > 20) {
+                    thisAmount += 10000 + 500 * (audience - 20);
+                }
+                thisAmount += 300 * audience;
+                break;
+            default:
+                throw new Error(`Tipo desconocido: ${playType}`);
+        }
+        return thisAmount;
     }
 }
